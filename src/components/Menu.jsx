af9458c558e02567a37819cf44de8daa34303513
Joao <joao.mont@ua.pt>
2023-05-03 19:33:45 +0100

    bit e commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+
+function Menu() {
+
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default Menu
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      main:{
+        black : '#000',
+        white : '#fff',
+        grey : '#595758',
+        red : '#E26D5C',
+        green : '#5FAD56',
+      }
+    },
+    fontFamily:{
+      display : ['Segoe UI'],
+      body : ['Segoe UI'], 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

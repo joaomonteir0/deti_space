4710598a17aed8f62dca8d9a00406006067f8b24
lilikas11 <lilianapcribeiro@gmail.com>
2023-05-05 19:45:02 +0100

    cores

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,3 +1,19 @@
+import './Help.css'
+
 export const Help = () => {
-    
+    return (
+        <div className="holder">
+            <div className="content">
+                <div className="topo">
+                    <div className = "sobreNos" >
+                        a
+                    </div>
+                    <div className = "links" >b
+                    </div>
+                </div>
+                <div className = "contato" >c
+                </div>
+            </div>
+        </div>
+    )
 }

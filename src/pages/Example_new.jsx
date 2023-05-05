2d312c9aa517b7b35a32fd9ce68a4bb01da18ef7
joao <joao@pop-os.localdomain>
2023-05-05 19:39:17 +0100

    pagina noticia individual e coisas

--- src/pages/Example_new.jsx
+++ src/pages/Example_new.jsx
@@ -4,9 +4,27 @@ export const Example_new = () => {
     return (
         <div className="holder">
             <div className="content">
-                <div className="title">a</div>
-                <div className="infos">b</div>
-                <div className="text">c</div>
+                <div className="img-new">
+                    <img src="src/assets/1.jpg" alt="" />
+                </div>
+                <div className="topo">
+                    <div className="title">Lorem ipsum dolor sit amet.</div>
+                    <div className="voltar">
+                        <a href="/news">« Voltar à pagina de notícias</a>
+                    </div>
+                </div>
+                <div className="infos">
+                    <div className="tags">
+                        <div className="tag bgred">DETI</div>
+                        <div className="tag bggreen">Students@DETI</div>
+                        <div className="tag bgblue">Eventos</div>
+                    </div>
+                </div>
+                <div className="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus, eveniet enim magnam provident nemo vitae ducimus quidem. Odio ut sit accusamus quae non explicabo, repellendus veritatis iusto, praesentium necessitatibus autem unde impedit. Reiciendis dolorum cum laudantium accusantium corporis! Soluta quod cupiditate necessitatibus iste ipsam, laboriosam, quae, tenetur dolorum laborum sit pariatur veritatis odio placeat quos. Totam, soluta ratione expedita quisquam autem veritatis eaque quo cumque iure placeat illo fugiat consequuntur porro exercitationem illum tempore, numquam ad vero quos deserunt distinctio ut suscipit! Saepe totam sit ducimus et! Qui inventore consequuntur ut similique animi consequatur id a. Autem, eligendi praesentium repellendus harum expedita ea ducimus corporis laborum ut amet saepe! Pariatur est illum ullam itaque commodi, quis nostrum fuga illo dolores voluptatibus officia tempora libero doloremque et totam ex dignissimos beatae unde. Eum minus nostrum odio possimus consectetur unde minima assumenda fuga nemo architecto, est dolores facere? Velit blanditiis officia sed ratione voluptas omnis, aperiam eaque! Veritatis et nulla porro assumenda impedit optio, minus possimus sapiente suscipit, doloribus modi! Ex excepturi debitis veritatis! Quasi molestiae corrupti repudiandae perspiciatis neque quia deleniti voluptatum temporibus dolores sed, reprehenderit quam blanditiis saepe vero odio sint odit error, id soluta maiores voluptatibus, doloribus nobis?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi repellendus fuga dignissimos molestiae recusandae magni ducimus sequi quisquam inventore tempora quam vero sunt similique eaque, facilis dolorem molestias suscipit est, numquam eligendi ipsam repellat, id dolorum. Maxime totam soluta dolor magni eaque saepe veritatis suscipit esse voluptatum ullam architecto voluptates laudantium quam nobis, provident ab quasi atque impedit similique numquam. Id ipsam, doloremque fugiat quos non quis, necessitatibus ipsum, tenetur laudantium ducimus velit blanditiis culpa quidem nulla porro voluptates? Aliquid eum hic ipsam dicta iusto veritatis nemo suscipit atque obcaecati aut? Numquam, odio cum ducimus voluptatum repellat voluptatibus sapiente id labore explicabo, molestias fuga unde nulla non iste enim libero, harum similique assumenda laboriosam. Ex voluptates, aperiam tenetur quidem sunt delectus nesciunt reprehenderit id maxime eligendi officia accusantium, exercitationem maiores, et ipsa voluptatum voluptatem quisquam aut! Laborum doloremque explicabo accusamus voluptatum laudantium iure ipsum fuga cum perspiciatis aliquid aut inventore, asperiores obcaecati iusto quaerat libero tempora odit expedita provident dolorum et repellat. Blanditiis sit ducimus saepe veniam quasi odio incidunt quod debitis soluta neque, minus reiciendis alias sequi nam, pariatur quibusdam reprehenderit ipsa vitae quaerat voluptatum ad nesciunt! Odit recusandae sit modi. Commodi cum quidem mollitia tempore temporibus doloribus reprehenderit vero nisi nihil tempora aspernatur modi iste nam necessitatibus natus consequuntur ut, atque laborum non repellendus earum. Asperiores iste nihil doloremque vero? Consectetur, animi quod incidunt earum ipsum dolores voluptatibus?</p>
+                </div>
+                .
             </div>
         </div>
     )

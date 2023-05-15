2416a262c62a84418d847bcebb611fff6a4b84f1
lilikas11 <lilianapcribeiro@gmail.com>
2023-05-16 00:07:18 +0100

    po caralho joao

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,38 +1,43 @@
 import userIcon from '../assets/Maria.png';
 import pencilImg from '../assets/pencil.png';
 import { useState } from 'react';
+import './UserPage.css'
 
 export const UserPage = () => {
-    const [readOnly,setReadOnly] = useState(true);
-    return(
-        <div className="container w-75" style={{marginTop: "5rem"}}>
+    const [readOnly, setReadOnly] = useState(true);
+    const [bioText, setBioText] = useState("Minha bio...");
+
+    const toggleEditMode = () => {
+        setReadOnly(!readOnly);
+    };
+
+    return (
+        <div className="container w-75" style={{ marginTop: "5rem" }}>
             <div className="row">
                 <div className="d-flex">
                     <div>
-                        <img src={userIcon} style={{width: "5rem"}}/>
+                        <img src={userIcon} style={{ width: "14rem" }} alt="User Icon" />
                     </div>
-                    <div className='ms-3'>
-                        <span className='fs-3'>Maria Ribeiro</span><br/>
-                        <span className='fs-5'>LECI</span>
+                    <div className="ms-3">
+                        <span className="fs-3">Maria Ribeiro</span><br />
+                        <span className="fs-5">LECI</span>
+                        <span className="fs-5">3ª Matricula</span>
                     </div>
                 </div>
             </div>
-            <div className="row mt-3 flex-nowrap">
-                <div className='position-relative'>
-                    <textarea className='form-control' readOnly={readOnly} style={{resize:"none"}}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus, earum tempora illo possimus id laborum inventore architecto exercitationem eveniet rem quidem officia, eum, voluptatibus nemo. Qui nisi harum enim!
-                    </textarea>
-                    <span className='position-absolute top-0 start-100 translate-middle'>
-                        <button className='btn' onClick={() => {setReadOnly(false)}}>
-                            <img src={pencilImg} style={{width: "1.5rem"}}/>
-                        </button>
-                    </span>
-                    <span className='position-absolute top-100 end-0 p-2' style={{visibility: readOnly ? 'hidden' : 'visible'}}>
-                        <button className='btn btn-primary' onClick={() => {setReadOnly(true)}}>
-                            OK
-                        </button>
-                    </span>
-                </div>
+            <div className="balao">
+                <p
+                    className={`texto ${readOnly ? "read-only" : ""}`}
+                    contentEditable={!readOnly}
+                    onInput={(event) => setBioText(event.target.textContent)}
+                >
+                    {bioText}
+                </p>
+                {!readOnly && (
+                    <button className="btn btn-success" onClick={toggleEditMode}>
+                        Salvar
+                    </button>
+                )}
             </div>
             <div className="row mt-5" id="Apontamentos">
                 <p className='fs-2'>Apontamentos</p>
@@ -53,7 +58,7 @@ function Note(props) {
                 <span className='border border-black rounded-circle fs-3 p-2'>{props.subject}</span>
             </div>
             <div className='ms-3'>
-                <span className='text-emphasis fs-4'>{props.description}</span><br/>
+                <span className='text-emphasis fs-4'>{props.description}</span><br />
                 <span className='badge border border-primary rounded text-black'>{props.date}</span>
                 <span className='badge border border-success rounded text-black ms-2'>{props.size}</span>
             </div>

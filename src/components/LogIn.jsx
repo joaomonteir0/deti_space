d8ac4023d8b0e01d6c689b7994ad89a43e407340
Joao <joao.mont@ua.pt>
2023-05-15 19:36:49 +0100

    bit and commit

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,13 @@
+import './Components.css';
+
 export const LogIn = (props) => {
 
     return (
         <div className="login">
-            <button className="position-absolute top-0 end-0 mt-3 me-3 btn btn-lg btn-primary" onClick={props.onClick}>LogIn</button>
+            <button className="login-button" onClick={props.onClick}>
+                <img src="src\assets\user.png" alt="" />
+                Entrar
+            </button>
         </div>
     )
 }

37e2492046bc8da4486d5adb43a8bdd90996af25
pompeucosta <pompeugcosta@gmail.com>
2023-05-22 14:01:08 +0100

    modal para sair

--- src/components/UserBox.jsx
+++ src/components/UserBox.jsx
@@ -4,21 +4,40 @@ import './Components.css';
 
 export const UserBox = (props) => {
     return (
+      <>
         <div className="user-box-container">
-        <div className="user-info">
-          <span className="user-name">Maria Ribeiro</span>
-          <div className="user-buttons">
-            <span>
-              <Link to="/user">
-                <button className="btn btn-primary me-2">Opcoes</button>
-              </Link>
-            </span>
-            <button className="btn btn-danger" onClick={props.onClick}>Sair</button>
+          <div className="user-info">
+            <span className="user-name">Maria Ribeiro</span>
+            <div className="user-buttons">
+              <span>
+                <Link to="/user">
+                  <button className="btn btn-primary me-2">Opcoes</button>
+                </Link>
+              </span>
+              <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalSair">Sair</button>
+            </div>
+          </div>
+          <div className="user-icon">
+            <img src={userIcon} style={{width: "6em"}} alt="User Icon" />
           </div>
         </div>
-        <div className="user-icon">
-          <img src={userIcon} style={{width: "6em"}} alt="User Icon" />
+        <div className="modal" id="modalSair">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3 className="modal-title">Sair</h3>
+                <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+              </div>
+              <div className="modal-body">
+                <p className="fs-5">Tem a certeza que deseja sair?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Não</button>
+                <button type="button" className="btn btn-danger" onClick={props.onClick} data-bs-dismiss="modal">Sim</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </>
     )
 }
